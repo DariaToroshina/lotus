0061318109e081848655a64c88c8eb7b8b5a93da
Дарья Торошина <idaha@MacBook-Air-Dara.local>
2025-07-08 00:23:18 +0300

    add navigation

--- src/modules/header/Header.tsx
+++ src/modules/header/Header.tsx
@@ -1,10 +1,24 @@
+import Link from "next/link";
 import LogoFull from "../shared/icons/LogoFull";
 import styles from "./Header.module.css";
 
 const Header = () => {
-  return <header className={styles.header}>
-    <LogoFull/>
-  </header>;
+  return (
+    <header className={styles.header}>
+      <LogoFull />
+      <nav className={styles.navigation}>
+        <Link href={""} className={styles.link}>
+          Главная
+        </Link>
+        <Link href={""} className={styles.link}>
+          Преимущества
+        </Link>
+        <Link href={""} className={styles.link}>
+          Контакты
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
