42369a31715b94d5a8d7e4a240af692c5bd0357a
dshtiller <dshtiller@ourgapps.com>
2025-07-08 00:26:46 +0300

    add wrap to header

--- src/modules/header/Header.tsx
+++ src/modules/header/Header.tsx
@@ -5,18 +5,20 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <LogoFull />
-      <nav className={styles.navigation}>
-        <Link href={""} className={styles.link}>
-          Главная
-        </Link>
-        <Link href={""} className={styles.link}>
-          Преимущества
-        </Link>
-        <Link href={""} className={styles.link}>
-          Контакты
-        </Link>
-      </nav>
+      <div className={styles.content}>
+        <LogoFull />
+        <nav className={styles.navigation}>
+          <Link href={""} className={styles.link}>
+            Главная
+          </Link>
+          <Link href={""} className={styles.link}>
+            Преимущества
+          </Link>
+          <Link href={""} className={styles.link}>
+            Контакты
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 };
