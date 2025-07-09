a0408ffa2284bb38f3a7c85b281c0065c7d0ff69
Дарья Торошина <idaha@MacBook-Air-Dara.local>
2025-07-09 23:52:46 +0300

    add sectionUseLotus

---
+++ src/modules/sectionUseLotus/ui/Chip.tsx
@@ -0,0 +1,15 @@
+import styles from "./Chip.module.css";
+import { Text } from "../../shared/ui/text/Text";
+import { FC, ReactNode } from "react";
+
+type ChipProps = {
+  text: string;
+};
+
+export const Chip: FC<ChipProps> = ({ text }) => {
+  return (
+    <div className={styles.root}>
+      <Text size="small">{text}</Text>
+    </div>
+  );
+};
---
+++ src/modules/sectionUseLotus/ui/SectionUseLotus.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styles from "./SectionUseLotus.module.css";
+import { Text } from "../../shared/ui/text/Text";
+import Subtitle from "../../shared/ui/subtitle/Subtitle";
+import { Button } from "../../shared/ui/button/Button";
+import { chipsConfig } from "./confiq/config";
+import { Chip } from "./Chip";
+
+const SectionUseLotus = () => {
+  return (
+    <section className={styles.root}>
+      <Subtitle>Где можно использовать Lotus RPA?</Subtitle>
+      <div className={styles.content}>
+        <div className={styles.text}>
+          <Text tag={"p"} weight="thin">
+            <Text tag={"span"} weight="bold">
+              Lotus RPA{" "}
+            </Text>
+            легко встроится в самые
+            <Text tag={"span"} weight="bold">
+              {" "}
+              различные{" "}
+            </Text>
+            бизнес-процессы:
+          </Text>
+        </div>
+      </div>
+      <div className={styles.chips}>
+        <div className={styles.row}>
+          {chipsConfig.slice(0, 8).map((chip, i) => (
+            <Chip text={chip.text} key={`chip-top-${i}`} />
+          ))}
+        </div>
+        {chipsConfig.length > 8 && (
+          <div className={styles.row}>
+            {chipsConfig.slice(8).map((chip, i) => (
+              <Chip text={chip.text} key={`chip-bottom-${i}`} />
+            ))}
+          </div>
+        )}
+      </div>
+
+      <Subtitle>Есть ли сферы, где RPA неприменим?</Subtitle>
+      <div className={styles.content}>
+        <div className={styles.text}>
+          <Text tag={"p"} weight="thin">
+            Мы полагаем, что таких сфер
+            <Text tag={"span"} weight="bold">
+              не существует.{" "}
+            </Text>
+            Заполните форму и мы расскажем об этом
+          </Text>
+          <Button className={styles.button}>Заполнить форму</Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SectionUseLotus;
