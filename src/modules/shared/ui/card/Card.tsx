d67abafcd7c4cccded49bc64d05973287c33dfe2
dshtiller <dshtiller@ourgapps.com>
2025-07-13 01:14:17 +0300

    refactor section process

--- src/modules/shared/ui/card/Card.tsx
+++ src/modules/shared/ui/card/Card.tsx
@@ -1,74 +1,98 @@
+// import clsx from "clsx";
+// import { FC, ReactNode } from "react";
+// import { Text } from "../text/Text";
+// import styles from "./Card.module.css";
+// import { CheckIcon } from "../../../sectionProcess/icon/CheckIcon";
+
+// type CardProps = {
+//   title: string;
+//   num?: ReactNode;
+//   variant?: "default" | "wide";
+//   children?: ReactNode;
+// };
+
+// export const Card: FC<CardProps> = ({
+//   title,
+//   variant = "default",
+//   children,
+//   num,
+// }) => {
+//   return (
+//     <div className={clsx(styles.root, styles[variant])}>
+//       <div className={styles.header}>
+//         {num}
+//         <Text className={styles.text} weight="bold">
+//           {title}
+//         </Text>
+//       </div>
+
+//       <div className={styles.description}>
+//         {children}
+//         {/* {isCustom ? (
+//           children
+//         ) : (
+//           <>
+//             {description && (
+//               <Text className={styles.text} size="small">
+//                 {description}
+//               </Text>
+//             )}
+
+//             {(text || button) && (
+//               <div className={styles.priceBlock}>
+//                 {text && <p className={styles.text}>{text}</p>}
+//                 {button && <div className={styles.buttonWrapper}>{button}</div>}
+//               </div>
+//             )}
+
+//             {checklist && checklist.length > 0 && (
+//               <ul className={styles.list}>
+//                 {checklist.map((item, i) => (
+//                   <li key={i} className={styles.wrapper}>
+//                     <CheckIcon className={styles.icon} /> {item}
+//                   </li>
+//                 ))}
+//               </ul>
+//             )}
+//           </>
+//         )} */}
+//       </div>
+//     </div>
+//   );
+// };
+
 import clsx from "clsx";
 import { FC, ReactNode } from "react";
 import { Text } from "../text/Text";
 import styles from "./Card.module.css";
-import { CheckIcon } from "../../../sectionProcess/icon/CheckIcon";
 
 type CardProps = {
-  step?: number;
-  title?: string;
-  description?: string;
-  text?: ReactNode;
-  checklist?: string[];
-  button?: ReactNode;
+  title: string;
+  description: string;
+  num?: ReactNode;
   variant?: "default" | "wide";
   children?: ReactNode;
 };
 
 export const Card: FC<CardProps> = ({
-  step,
   title,
   description,
-  text,
-  checklist,
-  button,
   variant = "default",
   children,
+  num,
 }) => {
-  const isCustom = Boolean(children);
-
   return (
     <div className={clsx(styles.root, styles[variant])}>
-      {!isCustom && (step || title) && (
-        <div className={styles.header}>
-          {step && <div className={styles.circle}>{step}</div>}
-          {title && (
-            <Text className={styles.text} weight="bold">
-              {title}
-            </Text>
-          )}
-        </div>
-      )}
+      <div className={styles.header}>
+        {num}
+        <Text className={styles.text} weight="bold">
+          {title}
+        </Text>
+      </div>
 
-      <div className={styles.description}>
-        {isCustom ? (
-          children
-        ) : (
-          <>
-            {description && (
-              <Text className={styles.text} size="small">
-                {description}
-              </Text>
-            )}
-
-            {(text || button) && (
-              <div className={styles.priceBlock}>
-                {text && <p className={styles.text}>{text}</p>}
-                {button && <div className={styles.buttonWrapper}>{button}</div>}
-              </div>
-            )}
-
-            {checklist && checklist.length > 0 && (
-              <ul className={styles.list}>
-                {checklist.map((item, i) => (
-                  <li key={i} className={styles.wrapper}>
-                    <CheckIcon className={styles.icon} /> {item}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </>
-        )}
+      <div className={styles.content}>
+        <Text size="small">{description}</Text>
+        {children}
       </div>
     </div>
   );
