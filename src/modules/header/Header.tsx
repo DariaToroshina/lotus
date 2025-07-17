7c08a825064f405b24f816ec24b0b4666fd93194
Дарья Торошина <toroshina.d.akme@gmail.com>
2025-07-17 12:01:19 +0300

    WIP: временные правки

--- src/modules/header/Header.tsx
+++ src/modules/header/Header.tsx
@@ -1,21 +1,38 @@
+"use client";
+
 import Link from "next/link";
 import LogoFull from "../shared/icons/LogoFull";
 import styles from "./Header.module.css";
+import { useState } from "react";
 
 const navigationConfig = [
   { label: "Главная", href: "#home" },
   { label: "Преимущества", href: "#advantages" },
-  { label: "Контакты", href: "#contacts" },
+  { label: "Контакты", href: "#form" },
 ];
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className={styles.root}>
       <div className={styles.content}>
-        <LogoFull />
-        <nav className={styles.navigation}>
+        <div className={styles.logo}>
+          <LogoFull />
+        </div>
+        <div className={styles.burger} onClick={() => setIsOpen(!isOpen)}>
+          <span />
+          <span />
+          <span />
+        </div>
+        <nav className={`${styles.navigation} ${isOpen ? styles.open : ""}`}>
           {navigationConfig.map((link, i) => (
-            <Link key={i} href={link.href} className={styles.link}>
+            <Link
+              key={i}
+              href={link.href}
+              className={styles.link}
+              onClick={() => setIsOpen(false)}
+            >
               {link.label}
             </Link>
           ))}
