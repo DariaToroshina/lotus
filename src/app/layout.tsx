fe274bf05fd04473f47ee2386d43e6aa99c96662
Дарья Торошина <toroshina.d.akme@gmail.com>
2025-07-30 18:53:00 +0530

    add metrica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import localFont from "next/font/local";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "LotusRPA",
@@ -43,7 +44,42 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={geometria.variable}>{children}</body>
+      <head>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) return;
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+              k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=103538616", "ym");
+
+            ym(103538616, "init", {
+              ssr: true,
+              webvisor: true,
+              clickmap: true,
+              ecommerce: "dataLayer",
+              accurateTrackBounce: true,
+              trackLinks: true
+            });
+          `}
+        </Script>
+      </head>
+      <body className={geometria.variable}>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/103538616"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
